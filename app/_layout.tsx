ec5a25e6c5098edd14c2767a0b977e0f2edd7830
elias.borja <elias.borja@centralshop.com.py>
2026-06-08 17:56:49 -0300

    Actualizacion

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { Slot } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
+import { View } from "react-native";
+import "./global.css";
+
 
 export default function RootLayout() {
-  return <Stack />;
+
+  useFonts({
+    "WorkSans-Black": require('../font/WorkSans-Black.ttf'),
+    "WorkSans-Light": require('../font/WorkSans-Light.ttf'),
+    "WorkSans-Medium": require('../font/WorkSans-Medium.ttf')
+  })
+  
+  return (
+    <View className="flex-1">
+      <Slot />
+      <StatusBar style="light" />
+    </View>
+  );
 }
